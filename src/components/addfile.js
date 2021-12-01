24b42480079f1bc1df231c917eeec890f0bf51e4
kumar <kumarvermaankit9555@gmail.com>
2021-12-01 15:05:06 +0530

    recorder

--- src/components/addfile.js
+++ src/components/addfile.js
@@ -1187,7 +1187,7 @@ function File(props) {
             <div>
               <h3 style={{ fontSize: "13px", marginLeft: "40px" }}>Click Twice to Open </h3>
               <h3 style={{ fontSize: "13px", marginLeft: "40px" }}>Give Permission to both screen and mic to record</h3>
-              {/* <Recorder /> */}
+              <Recorder />
             </div>
 
 
